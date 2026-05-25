518faf83382a7f023e7bed4a9fdf239bbc359f5a
shreyanshu-03 <s.shreyanshu@arcofintech.com>
2026-05-25 18:21:58 +0530

    add comments table model

--- app/models.py
+++ app/models.py
@@ -24,3 +24,15 @@ class Post(Base):
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)  # FK
 
     author = relationship("User", back_populates="posts")
+    
+class Comment(Base):
+    __tablename__ = "comments"
+ 
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(Text, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    post_id = Column(Integer, ForeignKey("posts.id"), nullable=False)
+ 
+    author = relationship("User")
+    post = relationship("Post")
+    
